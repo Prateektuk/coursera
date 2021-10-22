2c27e1c49fe0537691b2a6111a34acc40b68e22b
Prateektuk <91743041+Prateektuk@users.noreply.github.com>
2021-10-22 15:38:47 +0530

    Add files via upload

---
+++ bi.js
@@ -0,0 +1,48 @@
+console.log("hello world") 
+function abc(){
+    console.log("hi! kesa hai");
+}
+abc();
+let k = 45;
+console.log(typeof k)
+k = 'string';
+console.log(typeof k)
+k = true;
+console.log(typeof k)
+console.log(typeof abc);
+let abcdef;
+console.log(typeof abcdef);
+let gg = null;
+console.log(typeof gg);
+const fg = 25;
+let c =35;
+if(123 ==="123") console.log("hhhh");
+let ms = "qwerty";
+ms[2] = 'l';
+console.log(ms)
+
+ms.toUpperCase();
+console.log(ms)
+ms = "ankit";
+console.log(ms)
+let nam = [1,2,'pyiushe',true]
+console.log(nam)
+console.log(nam[1])
+nam.push("clare")
+console.log(nam)
+nam.unshift("clare")
+console.log(nam)
+console.log(nam.length)
+let p = nam.pop("clare")
+console.log(nam)
+console.log(p)
+let num = [1,2,3,4,5,6,7];
+
+})
+let sq = n.map(function(n){
+    return n*n;
+})
+console.log(sq)
+.slice
+.split
+
